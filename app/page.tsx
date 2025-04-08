6a6087f6675bfda2ecd8854e6f1d9891ab15a910
CodeWithClinton <clintonmatics@gmail.com>
2025-04-08 10:23:28 +0100

    added the product detail page

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const HomePage = () => {
     <>
     <Hero />
     <CategorySection />
-    <ProductSection />
+    <ProductSection title="Featured Products" />
     </>
   )
 }
--- components/home/ProductSection.tsx
+++ components/home/ProductSection.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ProductCard from "./ProductCard";
 
-const ProductSection = () => {
+interface Props{
+  title: string
+}
+
+const ProductSection = ({title}: Props) => {
   return (
     <section className="main-max-width padding-x mx-auto my-16">
       <h2 className="my-9 text-center text-xl font-bold text-gray-800">
-        Featured Products
+        {title}
       </h2>
 
       {/* Content */}
